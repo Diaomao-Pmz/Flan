c7e7fba5c0ed0a04a4f73192ed64a944263d24e9
kiriloler <kiriloler126@outlook.com>
2026-09-04 13:48:49 -0400

    符卡制作
    新增了三个符卡

---
+++ Assets/Scripts/Enemies/Boss/DataAssets/BulletNode/PatternScripts/ChaoticPattern.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewChaoticPattern", menuName = "ScriptableObjects/BulletPattern/Chaotic")]
+public class ChaoticPattern : BulletPatternBase
+{
+    [Header("--- Chaos ---")]
+    public float minSpeed;
+    public float maxSpeed;
+    public int bulletCountPerBurst;
+
+    public override void Spawn(BulletSpawnContext ctx)
+    {
+        for (int i = 0; i < bulletCountPerBurst; i++)
+        {
+            float speed = Random.Range(minSpeed, maxSpeed);
+            Vector2 dir = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+            ctx.host.SpawnProjectile(dir, ctx.firePoint.position, ctx.projectileKey, speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Boss/DataAssets/BulletNode/PatternScripts/CircularSectorPattern.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCircularSectorPattern", menuName = "ScriptableObjects/BulletPattern/CircularSector")]
+public class CircularSectorPattern : BulletPatternBase
+{
+    [System.Serializable]
+    public struct Bullet
+    {
+        public float speed;
+    }
+
+    [Header("--- Sector ---")]
+    public Bullet[] sectorBulletPerSection;
+    public int sectionCount = 18;
+    public float sectorOffsetY = 0f; // 圆环作为整体，依然保留出生点偏移
+    public float angleRange = 90f;
+
+    public override void Spawn(BulletSpawnContext ctx)
+    {
+        Vector3 spawnPos = ctx.firePoint.position + new Vector3(0, sectorOffsetY, 0);
+        float angleStep = angleRange / sectionCount;
+
+        float targetAngle = Vector2.SignedAngle(new Vector2(1, 0), ctx.playerTargetPosition - (Vector2)spawnPos);
+
+        float startAngle = targetAngle - angleRange / 2;
+
+        for (int i = 0; i < sectionCount; i++)
+        {
+            float currentAngle = startAngle + i * angleStep;
+            Vector2 dir = new Vector2(Mathf.Cos(currentAngle * Mathf.Deg2Rad), Mathf.Sin(currentAngle * Mathf.Deg2Rad));
+            for(int j = 0; j < sectorBulletPerSection.Length; j++)
+            {
+                ctx.host.SpawnProjectile(dir, spawnPos, ctx.projectileKey, sectorBulletPerSection[j].speed);
+            }
+        }
+    }
+}
